b681a1dc03a1268e577c2c4d9c88ae8e97cf9549
someone5678 <someone5678@users.noreply.github.com>
2023-10-18 17:33:54 +0900

    build: Erase resource-cahce and persistent_properties upon flashing
    * Properly update persistent properties
    * Avoid resources bug
    
    Change-Id: I7274930d32d62e7c7a8e7a3a4ac66de23f293cbd

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -3609,6 +3609,15 @@ class BlockDifference(object):
                 new_data_name=new_data_name, code=code))
     script.AppendExtra(script.WordWrap(call))
 
+    call = ('delete_recursive("/data/system/package_cache");')
+    script.AppendExtra(script.WordWrap(call))
+
+    call = ('delete_recursive("/data/resource-cache");')
+    script.AppendExtra(script.WordWrap(call))
+
+    call = ('delete_recursive("/data/property/persistent_properties");')
+    script.AppendExtra(script.WordWrap(call))
+
   def _HashBlocks(self, source, ranges):  # pylint: disable=no-self-use
     data = source.ReadRangeSet(ranges)
     ctx = sha1()
